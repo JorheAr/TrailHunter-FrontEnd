4e9421281792274b13464917b464ea5ca12c9ffe
Jorge Arias <jorgeariasmartin3@gmail.com>
2025-04-23 12:02:51 +0200

    ✨ Add methods getFollowStatsById, getUsuarios, getUsuarioById

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -10,6 +10,20 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
+  getUsuarios(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : {};
+
+    return this.http.get<any>(`${this.apiUrl}/all`, { headers });
+  }
+
+  getUsuarioById(id: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : {};
+
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers });
+  }
+
 
   getUsuarioActual(): Observable<any> {
     const token = localStorage.getItem('token');
@@ -40,5 +54,30 @@ export class UserService {
           throw error;
         })
       );
+  }
+
+  getFollowStatsById(userId: number): Observable<{ seguidores: number; seguidos: number }> {
+    const token = localStorage.getItem('token');
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : {};
+
+    return this.http.get<{ seguidores: number; seguidos: number }>(
+      `${this.apiUrl}/${userId}/follow-stats`,
+      { headers }
+    );
+  }
+
+
+  followUser(userId: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : {};
+
+    return this.http.post<any>(`${this.apiUrl}/follow`, { user_id: userId }, { headers });
+  }
+
+  unfollowUser(userId: number): Observable<any> {
+    const token = localStorage.getItem('token');
+    const headers = token ? new HttpHeaders().set('Authorization', `Bearer ${token}`) : {};
+
+    return this.http.post<any>(`${this.apiUrl}/unfollow`, { user_id: userId }, { headers });
   }
 }
