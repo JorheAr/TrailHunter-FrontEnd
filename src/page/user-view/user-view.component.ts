57c4a953aa6533e0d02e3a545a8d0e51d29d0fe6
Jorge Arias <jorgeariasmartin3@gmail.com>
2025-04-23 12:00:11 +0200

    ✨ Create user view

---
+++ src/page/user-view/user-view.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService} from '../../services/user.service';
+import {NgClass, NgIf} from '@angular/common';
+import {LoaderComponent} from '../../component/loader/loader.component';
+
+@Component({
+  selector: 'app-user-view',
+  templateUrl: './user-view.component.html',
+  imports: [
+    NgClass,
+    NgIf,
+    LoaderComponent
+  ],
+  standalone: true
+})
+export class UserViewComponent implements OnInit {
+  usuario: any;
+  cargando = true;
+  seguidores = 0;
+  seguidos = 0;
+  avatarUrl = '';
+  userId!: number;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.userId = Number(this.route.snapshot.paramMap.get('id'));
+    this.cargarUsuario();
+  }
+
+  cargarUsuario() {
+    this.userService.getUsuarioById(this.userId).subscribe({
+      next: (res) => {
+        this.usuario = res;
+        this.avatarUrl = `https://api.dicebear.com/6.x/lorelei-neutral/png?seed=${res.username}`;
+        this.userService.getFollowStatsById(this.userId).subscribe({
+          next: (stats) => {
+            this.seguidores = stats.seguidores;
+            this.seguidos = stats.seguidos;
+          },
+          error: (err) => {
+            console.error('Error cargando estadísticas de seguidores', err);
+          }
+        });
+
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error('Error cargando usuario', err);
+        this.cargando = false;
+      },
+    });
+  }
+
+  seguirUsuario() {
+    this.userService.followUser(this.userId).subscribe(() => {
+      this.usuario.is_following = true;
+      this.actualizarStats();
+    });
+  }
+
+  dejarDeSeguirUsuario() {
+    this.userService.unfollowUser(this.userId).subscribe(() => {
+      this.usuario.is_following = false;
+      this.actualizarStats();
+    });
+  }
+
+  actualizarStats() {
+    this.userService.getFollowStatsById(this.userId).subscribe({
+      next: (stats) => {
+        this.seguidores = stats.seguidores;
+        this.seguidos = stats.seguidos;
+      },
+      error: (err) => {
+        console.error('Error actualizando estadísticas', err);
+      }
+    });
+  }
+
+}
