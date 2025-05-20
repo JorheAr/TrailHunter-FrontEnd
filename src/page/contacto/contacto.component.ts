e24255c8e28dae236929eff7eb79295f334d8be0
Jorge Arias <jorgeariasmartin3@gmail.com>
2025-05-20 09:39:39 +0200

    💄 Changes on front

--- src/page/contacto/contacto.component.ts
+++ src/page/contacto/contacto.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
-  templateUrl: './contacto.component.html'
+  templateUrl: './contacto.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf
+  ]
 })
 export class ContactoComponent {
+  nombre: string = '';
+  correo: string = '';
+  mensaje: string = '';
+  enviado: boolean = false;
+  error: string | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  enviarFormulario() {
+    const payload = {
+      nombre: this.nombre,
+      correo: this.correo,
+      mensaje: this.mensaje
+    };
+
+    this.http.post('http://localhost:5000/contacto', payload).subscribe({
+      next: () => {
+        this.enviado = true;
+        this.error = null;
+        this.nombre = '';
+        this.correo = '';
+        this.mensaje = '';
+      },
+      error: (err) => {
+        this.error = err.error?.error || 'Error al enviar el mensaje.';
+      }
+    });
+  }
 }
